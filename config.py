b301498b889d8c9fe3be90e1b9fa1010f5f33cfd
OsGugas os NÂO Pato <78082669+Gusgas@users.noreply.github.com>
2026-01-21 16:11:22 -0300

    Add validation for environment variables
    Added a check to ensure environment variables are set.

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 MP_ACCESS_TOKEN = os.environ.get("MP_ACCESS_TOKEN")
 GROUP_ID = os.environ.get("GROUP_ID")
+
+# Checagem rápida para garantir que tudo foi carregado
+if not BOT_TOKEN or not MP_ACCESS_TOKEN or not GROUP_ID:
+    raise ValueError("⚠️ Variáveis de ambiente não configuradas corretamente!")
